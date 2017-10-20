c65e8ea9817bc05dabc0012d2bf4d436462f2b12
andy saputra <andygrap@gmail.com>
2017-10-21 00:13:09 +0700

    first commit

---
+++ Pinterin/view/slide.swift
@@ -0,0 +1,21 @@
+//
+//  slide.swift
+//  Pinterin
+//
+//  Created by andyptra on 10/20/17.
+//  Copyright © 2017 andyptra. All rights reserved.
+//
+
+import UIKit
+
+class slide: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
